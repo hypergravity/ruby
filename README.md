9427cf1e893201c75e4abd4a7f4d6b136bc0d335
Bo Zhang <bzhang@mpia.de>
2018-04-27 10:39:14 +0200

    updated README and setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# ruby
-bling bling
+# RUBY
+
+For Bayesian spectroscopy.
+
+
+`pip install git+https://github.com/hypergravity/ruby.git`
+or
+`pip install ruby`
---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+
+if __name__ == '__main__':
+    setup(
+        name='ruby',
+        version='0.0.1',
+        author='Bo Zhang',
+        author_email='bzhang@mpia.de',
+        # py_modules=['bopy','spec','core'],
+        description='For Bayesian spectroscopy.',  # short description
+        license='MIT',
+        # install_requires=['numpy>=1.7','scipy','matplotlib','nose'],
+        url='http://github.com/hypergravity/ruby',
+        classifiers=[
+            "Development Status :: 6 - Mature",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.6",
+            "Topic :: Scientific/Engineering :: Astronomy",
+            "Topic :: Scientific/Engineering :: Physics"],
+        package_dir={'ruby/': ''},
+        packages=['ruby', ],
+        package_data={'ruby/data': [''],
+                      "":          ["LICENSE"]},
+        include_package_data=True,
+        requires=['numpy', 'scipy', 'matplotlib', 'astropy', 'joblib']
+    )
