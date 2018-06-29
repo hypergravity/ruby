732e9262872371dace3cd7369a33214e4fc7f01e
Bo Zhang <bzhang@mpia.de>
2018-06-29 15:07:17 +0200

    added package data directory

--- setup.py
+++ setup.py
@@ -20,11 +20,9 @@ if __name__ == '__main__':
             "Programming Language :: Python :: 3.6",
             "Topic :: Scientific/Engineering :: Astronomy",
             "Topic :: Scientific/Engineering :: Physics"],
-        package_dir={'ruby/': ''},
+        package_dir={'ruby': 'ruby'},
         packages=['ruby', ],
-        package_data={'ruby/data': [''],
-                      'ruby/script': [''],
-                      "":          ["LICENSE"]},
+        package_data={"ruby": ["ruby/data/*", "ruby/script/*"]},
         include_package_data=True,
         requires=['numpy', 'scipy', 'matplotlib', 'astropy', 'joblib']
     )
